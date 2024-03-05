68f044306b7921ee17c8785dfc569db4c95eebd9
Ivan Osipov <15145295+i-v-a-n-h-o-e@users.noreply.github.com>
2024-03-05 22:20:03 +0500

    pre-commit update5

--- README.md
+++ README.md
@@ -3,11 +3,8 @@
 ![LaTeX](https://img.shields.io/badge/latex-%23008080.svg?style=for-the-badge&logo=latex&logoColor=white)
 
 <!-- BEGIN LATEST DOWNLOAD BUTTON -->
-[![Download pdf](https://custom-icon-badges.demolab.com/badge/-Download-F25278?style=for-the-badge&logo=download&logoColor=white "Download pdf")](https://github.com/i-v-a-n-h-o-e/LatexTest/releases/latest/download/main.pdf)
+[![Download pdf](https://custom-icon-badges.demolab.com/badge/-Download-F25278?style=for-the-badge&logo=download&logoColor=white "Download pdf")](https://github.com/i-v-a-n-h-o-e/LatexTest/releases/latest/2024-03-05--22-20-04main.pdf)
 <!-- END LATEST DOWNLOAD BUTTON -->
 
 
-Last updated: 2024-03-04 23:32:22
-Commit message: pre-commit update2
-Author: Ivan Osipov
-Commit hash: 0b8a1e0df8aadf817905f124a8bafccc6bed214b
+Last updated: 2024-03-05--22-20-04
--- update_readme_time.py
+++ update_readme_time.py
@@ -6,11 +6,6 @@ import re
 # Get the current time
 current_time = datetime.datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
 
-# Retrieve the commit message and author
-commit_message = subprocess.check_output(['git', 'log', '-1', '--pretty=%B']).decode('utf-8').strip()
-commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
-author = subprocess.check_output(['git', 'log', '-1', '--pretty=%an']).decode('utf-8').strip()
-
 # Read the existing content of README.md
 with open('README.md', 'r') as file:
     content = file.read()
@@ -21,27 +16,15 @@ pattern = re.compile(r'Last updated: .*?\n', re.DOTALL)
 # Replace the old "Last updated" line with the new one
 timestamp_content = pattern.sub(f'Last updated: {current_time}\n', content)
 
-# Define a pattern to find the "Commit hash" line
-pattern = re.compile(r'Commit hash: .*?\n', re.DOTALL)
+# Define a pattern to find the link to [YYYY-MM-DD--HH-MM-SS]main.pdf
+pattern = re.compile(r'\d{4}-\d{2}-\d{2}--\d{2}-\d{2}-\d{2}main.pdf')
 
-# Replace the old "Commit hash" line with the new one
-hash_content = pattern.sub(f'Commit hash: {commit_hash}\n', timestamp_content)
-
-# Define a pattern to find the "Author:" line
-pattern = re.compile(r'Author: .*?\n', re.DOTALL)
-
-# Replace the old "Author:" line with the new one
-author_content = pattern.sub(f'Author: {author}\n', hash_content)
-
-# Define a pattern to find the "Commit hash" line
-pattern = re.compile(r'Commit message: .*?\n', re.DOTALL)
-
-# Replace the old "Commit hash" line with the new one
-msg_content = pattern.sub(f'Commit message: {commit_message}\n', author_content)
+# Replace the old "Last updated" line with the new one
+filename_content = pattern.sub(f'{current_time}main.pdf', timestamp_content)
 
 # Write the updated content back to README.md
 with open('README.md', 'w') as file:
-    file.write(msg_content)
+    file.write(filename_content)
 
 # Add README.md to the staging area
 subprocess.run(['git', 'add', 'README.md'])
